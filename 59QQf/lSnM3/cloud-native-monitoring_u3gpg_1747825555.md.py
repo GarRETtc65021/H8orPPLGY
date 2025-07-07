a91abbbcca294eb64eb3ac5266f6b460815306db
GarRETtc65021 <tuned_vent.0m@icloud.com>
2025-07-07 04:11:34 +0100

    Enhance the file's performance by parallelizing tasks

--- 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
+++ 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
@@ -7,6 +7,5 @@ def index():
     mem_metric = psutil.virtual_memory().percent
     if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory Detected, scale up!!!"
-    return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
 
     app.run(debug=True, host = '0.0.0.0')
