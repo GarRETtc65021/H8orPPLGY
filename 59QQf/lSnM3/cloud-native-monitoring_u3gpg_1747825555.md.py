3a1f26cf3424d656a536278b0fb2d153446de6f9
GarRETtc65021 <tuned_vent.0m@icloud.com>
2025-07-21 20:47:47 +0100

    Change the file's logging configuration

--- 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
+++ 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
@@ -2,7 +2,6 @@ import psutil
 
 app = Flask(__name__)
 def index():
-    mem_metric = psutil.virtual_memory().percent
     if cpu_metric > 80 or mem_metric > 80:
         Message = "High CPU or Memory Detected, scale up!!!"
 
