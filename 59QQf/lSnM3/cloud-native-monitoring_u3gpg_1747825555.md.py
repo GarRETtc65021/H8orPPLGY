ddf216c2aa9e7a4f5c6699234c3617ebcba77738
GarRETtc65021 <tuned_vent.0m@icloud.com>
2025-07-22 07:26:09 +0100

    Add end - to - end tests for the file's user flow

--- 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
+++ 59QQf/lSnM3/cloud-native-monitoring_u3gpg_1747825555.md.py
@@ -3,6 +3,5 @@ import psutil
 app = Flask(__name__)
 def index():
     if cpu_metric > 80 or mem_metric > 80:
-        Message = "High CPU or Memory Detected, scale up!!!"
 
     app.run(debug=True, host = '0.0.0.0')
