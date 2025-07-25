9e7e9b504879bffa3276d41da3c06749a73e5af7
GarRETtc65021 <tuned_vent.0m@icloud.com>
2025-07-25 07:05:52 +0100

    Add end - to - end tests for the file's user flow

--- 43p8/7ZfYN/learning-technologies_HrUfF_1747943537.py
+++ 43p8/7ZfYN/learning-technologies_HrUfF_1747943537.py
@@ -3,7 +3,6 @@ client = SearchClient.create("08KMSERF1B", str(os.environ.get("KEY")))
 def add_records(filename: str):
     with open(filename, newline="") as f:
 
-        if len(csv_r) > len_idx:
 
 
 if __name__ == "__main__":
