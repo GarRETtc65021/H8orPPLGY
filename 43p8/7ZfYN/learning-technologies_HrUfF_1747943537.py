d7a12e74c6ca84fcf2a5444b327ce421424964fb
GarRETtc65021 <tuned_vent.0m@icloud.com>
2025-07-25 07:58:47 +0100

    Add comments to explain complex logic in the file

--- 43p8/7ZfYN/learning-technologies_HrUfF_1747943537.py
+++
@@ -1,9 +0,0 @@
-
-client = SearchClient.create("08KMSERF1B", str(os.environ.get("KEY")))
-def add_records(filename: str):
-    with open(filename, newline="") as f:
-
-
-
-if __name__ == "__main__":
-    add_records("projects.csv")
